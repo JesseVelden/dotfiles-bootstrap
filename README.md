e1bd54b7d15ae56d33a3a3a57df1007d0795ebc8
Jesse van der Velden <mail@jessevandervelden.nl>
2018-11-11 15:22:55 +0100

    First commit

---
+++ README.md
@@ -0,0 +1,84 @@
+<h1 align="center">My . files ❤ ~/ - Bootstrap</h1>
+This is my setup to be productive on MacOS, Ubuntu/ Debian like systems and even
+Windows 10. This is the bootstrap part to setup the stuff before to get the
+actual dotfiles.
+
+# Installation
+The main part is to setup git and SSH before we can proceed with further
+installation of the dotfiles.
+## MacOS
+Download Xcode in the AppStore and open in the terminal:
+```bash
+xcode-select --install
+```
+Then setup the SSH keys and add it to Github. Next:
+```bash
+git clone git@github.com:MegaCookie/dotfiles.git ~/dotfiles && cd ~/dotfiles && ./install.sh
+```
+## Debian
+```bash
+sudo apt update && sudo apt upgrade && sudo apt install git
+```
+Then setup the SSH keys and add it to Github. Next:
+```bash
+git clone https://github.com/megacookie/dotfiles.git ~/dotfiles && cd ~/dotfiles && ./install.sh
+```
+## Windows 10
+We need to install git and SSH. Under Windows that is a bit difficult. Therefore
+a PowerShell script is made.
+
+Open an elevated Powershell (`Win` `x`  + `a`)
+```PowerShell
+Set-ExecutionPolicy Unrestricted
+```
+The following script installs git and add a SSH key to Github using the Github API.
+```PowerShell
+iex ((new-object net.webclient).DownloadString('https://raw.github.com/megacookie/dotfiles-bootstrap/master/setup.ps1'))
+```
+
+# Manual SSH config
+If you used the script above it should already be added to ~/.ssh/config and
+added to Github. If it fails or you want do it manually you can follow the
+procedure described here below.
+
+## Starting SSH agent
+### Mac/ Debian:
+```bash
+eval $(ssh-agent -s)
+```
+
+### Windows
+```PowerShell
+Set-Service ssh-agent -StartupType Manual
+Start-Service ssh-agent
+```
+
+## Generating SSH key
+On Windows 10/ MacOS/ Debian it is all the same:
+```bash
+ssh-keygen -t rsa -b 4096 -C "mail@jessevandervelden.nl" -f ~/.ssh/github_rsa
+```
+## Add to `~/.ssh/config`:
+```
+Host github.com
+  HostName github.com
+  PreferredAuthentications publickey
+  IdentityFile ~/.ssh/github_rsa
+  IdentitiesOnly yes
+```
+## Add to Github
+https://github.com/settings/keys
+### Mac
+```bash
+pbcopy < ~/.ssh/github_rsa.pub
+```
+### Debian
+```bash
+clip < ~/.ssh/github_rsa.pub
+```
+### Windows 10
+```PowerShell
+Get-Content ~\.ssh\github_rsa.pub | Set-Clipboard
+```
+# Enjoy!
+Enjoy and be productive!
---
+++ setup.ps1
@@ -0,0 +1,111 @@
+###****************************###
+### Install git and check SSH. ###
+###****************************###
+
+Write-Host "Installing Package Providers..." -ForegroundColor "Yellow"
+Get-PackageProvider NuGet -Force | Out-Null
+
+if (!(Test-Path -path "$env:ProgramData\Chocolatey")) {
+  Set-ExecutionPolicy Unrestricted; iex ((New-Object System.Net.WebClient).DownloadString('https://chocolatey.org/install.ps1'))
+}
+
+Write-Host "Installing PowerShell Modules..." -ForegroundColor "Yellow"
+if (!(Get-Module -ListAvailable -Name Posh-Git)) {
+  Install-Module Posh-Git -Scope CurrentUser -Force
+}
+if (!(Get-Module -ListAvailable -Name PSWindowsUpdate)) {
+  Install-Module PSWindowsUpdate -Scope CurrentUser -Force
+}
+
+if (!(Test-Path HKLM:Software\GitForWindows)) {
+  choco install git.install --limit-output -params '"/GitAndUnixToolsOnPath /NoShellIntegration"'
+}
+
+#curl.exe is actually available since 1804
+if (!((Test-Path -path "${env:ProgramData}\chocolatey\lib\curl") -and (Test-Path "${env:WinDir}\System32\curl.exe"))) {
+  choco install curl --limit-output
+}
+
+# Assuming openssh is installed since 1804
+Write-Host "Assuming OpenSSH is installed (default since 1804)" -ForegroundColor "Yellow"
+if (!(Test-Path "${env:WinDir}\System32\OpenSSH\ssh-keygen.exe")) {
+  Write-Host "${env:WinDir}\System32\OpenSSH\ssh-keygen.exe does not exsists. ABORT..." -ForegroundColor "Red"
+  exit
+  #Actually it can be installed with cocolatey but now i just don't support < 1804
+}
+
+
+###******************************************###
+### Generating SSH key and add it to github. ###
+###******************************************###
+Set-Service ssh-agent -StartupType Manual
+Start-Service ssh-agent
+
+if (!(Test-Path $HOME\.ssh\github_rsa)) {
+  $confirmation = Read-Host "Do you want to generate a new SSH key and add it to Github? [y/n]"
+  if ($confirmation -eq 'y') {
+    Write-Host "Trying to test the Github connection..." -ForegroundColor "Yellow"
+
+    ssh -q git@github.com # Test github connection. It will auto exit on its own at the server side.
+    if ($LASTEXITCODE -eq 255) {
+      $gitemail = Read-Host -Prompt 'Input your github email'
+
+      # Generating keys
+      ssh-keygen -t rsa -b 4096 -C "$gitemail" -f $HOME\.ssh\github_rsa
+
+      $gituser = Read-Host -Prompt 'Input your github username'
+
+      $twoauth = Read-Host "Do you have 2factor authentication enabled? [y/n]"
+      if ($twoauth -match "[yY]") {
+        $gitotp = Read-Host -Prompt 'Input your github otp'
+      }
+
+      # Adding to Github using their API...
+      $sshkey = [IO.File]::ReadAllText("$HOME\.ssh\github_rsa.pub")
+
+      # Create data as PS hashtable literal.
+      $json = @{ title = "WinTestje"; key = "$sshkey" }
+
+      if ($twoauth -match "[yY]") {
+        # Convert to JSON with ConvertTo-Json and pipe to `curl` via *stdin* (-d '@-')
+        $added = $json | ConvertTo-Json -Compress | curl.exe -i -s -u $gituser `
+          -H "X-GitHub-OTP: $gitotp"`
+          -H "Content-Type: application/json"`
+          -d '@-'`
+          https://api.github.com/user/keys | Select -First 1 | Select-String '201' -quiet
+      } else {
+        $added = $json | ConvertTo-Json -Compress | curl.exe -i -s -u $gituser `
+          -H "Content-Type: application/json"`
+          -d '@-'`
+          https://api.github.com/user/keys | Select -First 1 | Select-String '201' -quiet
+      }
+
+      if ($added) {
+        ###*******************************###
+        ### Adding to ~\.ssh\config file. ###
+        ###*******************************###
+        Write-Host "IT WORKED! :). Now we'll add it to: ~\.ssh\config" -ForegroundColor "Green"
+        if (!(Get-Content $HOME\.ssh\config  | Select-String -pattern "\bHostName github.com\b" -quiet)) {
+          Write-Host "Didn't find it yet in the config file. So addding it..." -ForegroundColor "Green"
+          Add-Content $HOME\.ssh\config "`nHost github.com`n`tHostName github.com`n`tPreferredAuthentications publickey`n`tIdentityFile ~/.ssh/github_rsa`n`tIdentitiesOnly yes"
+        } else {
+          Write-Host "There was already a hostname with github.com found! So didn't add it to: ~\.ssh\config!!" -ForegroundColor "Red"
+        }
+      }
+    } else {
+      Write-Host "Already connected to Github..." -ForegroundColor "Green"
+    }
+  }
+} else {
+  Write-Host "Already found a ~\.ssh\github_rsa file. You should be able to connect to github already. Follow the manual to proceed." -ForegroundColor "Green"
+}
+
+Write-Host "Trying to test the Github connection one more time..." -ForegroundColor "Yellow"
+ssh -q git@github.com # Test github connection. It will auto exit on its own at the server side.
+if ($LASTEXITCODE -eq 255) {
+  Write-Host "Cannot connect to Github. Not authorized... Add your own keys to Github using the manual." -ForegroundColor "Red"
+} else {
+  Write-Host "Successfully connected to Github! You can proceed with the dotfiles installation" -ForegroundColor "Green"
+}
+
+Write-Host "Hopefully everything worked!" -ForegroundColor "Yellow"
